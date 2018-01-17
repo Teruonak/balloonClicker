eccd3ed9e0ea996d10a7233d7b4b8b71416e07d0
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2018-01-17 00:12:05 -0200

    Updating phaser version to 2.9.4

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # balloonClicker
 click with your mouse to fill the balloon
+
+# Thanks to
+
+- AsylusGoji91 for Peach baloon [http://fav.me/d7qbno4]
--- js/balloon-clicker.js
+++ js/balloon-clicker.js
@@ -1,25 +1,25 @@
 var game = new Phaser.Game(400, 400, Phaser.AUTO, 'balloon-clicker', { preload: preload, create: create, update: update });
 
-var sprite;
-
 function preload() {
 
     //  37x45 is the size of each frame
     //  There are 18 frames in the PNG - you can leave this value blank if the frames fill up the entire PNG, but in this case there are some
     //  blank frames at the end, so we tell the loader how many to load
-    game.load.spritesheet('ms', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+    game.load.spritesheet('peach', 'assets/sprites/balloon_princess_peach_sprite_sheet_by_AsylusGoji91.png', 33, 61, 3);
 
 }
 
+var peach;
+var cursors;
+
 function create() {
 
-    sprite = game.add.sprite(40, 100, 'ms');
+    peach = game.add.sprite(40, 100, 'peach');
 
-    sprite.animations.add('walk');
+    peach.animations.add('move', [0,1,2], 5, true);
 
-    sprite.animations.play('walk', 50, true);
-
-    game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
 
 }
 
@@ -27,10 +27,15 @@ function create() {
 //  then create a function called loadUpdate() and use that
 function update() {
 
-    if (sprite.x >= 300)
-    {
-        sprite.scale.x += 0.01;
-        sprite.scale.y += 0.01;
+    if (cursors.left.isDown) {
+        peach.animations.play('move');
+    }
+    else if (cursors.right.isDown) {
+        peach.animations.play('move');
+    }
+    else {
+        peach.animations.stop();
+        peach.frame = 0;
     }
 
 }
